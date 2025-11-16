d0eadbcb27c9b9c1330df333af34ca08dcc1c091
Hailey Wong-Budiman <haileylwb@gmail.com>
2025-11-16 09:50:02 -0500

    fix: github link updated

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('.navButton');
     
     buttons[0].addEventListener('click', function() {
-        window.open('https://github.com/haileylwb/LockedIn', '_blank');
+        window.open('https://github.com/SerenaC0210/LockedIn', '_blank');
     });
     
     buttons[1].addEventListener('click', function() {
